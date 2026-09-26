af90066b7f5e3187108e51b5435b7e8855e4f3b5
Raquel-M325 <raquelmartiniano14@gmail.com>
2026-09-26 11:20:06 -0300

    corrigi a falha

--- AVL/arvoreAVLInterface.java
+++ AVL/arvoreAVLInterface.java
@@ -1,1 +1,8 @@
-0
+package AVL;
+
+public interface arvoreAVLInterface extends arvoreABP {
+   boolean isAVL();
+   int balancing(No node);
+   No rotationEsq(No node);
+   No rotationDir(No node);
+}   
